d17f3ffeaec0910fecf3228ad83efae6de1c4d0f
Adistian Herlambang <adistian59@gmail.com>
2026-05-17 15:41:57 +0700

    git

--- api/dashboard/overview.js
+++ api/dashboard/overview.js
@@ -1,5 +1,10 @@
 import e from "express";
 import axios from "axios";
+import dotenv from "dotenv";
+
+dotenv.config()
+
+//utils
 import batchProcessor from "../../utils/batchProcessor.js";
 
 const router = e.Router()
@@ -50,167 +55,85 @@ router.post("/ihk", async (req, res) => {
 })
 
 router.post("/kelompok", async (req, res) => {
-
   try {
-
-    const API_KEY =
-
-      "6140cf4d3d3cc537fe36176ad6ad09d2";
+    const API_KEY = process.env.BPS_API_KEY
 
     const kelompok = [
-
       {
-
         nama: "Makanan, Minuman dan Tembakau",
-
         var: 2223
-
       },
-
       {
-
         nama: "Pakaian dan Alas Kaki",
-
         var: 2224
-
       },
-
       {
-
         nama: "Perumahan, Air, Listrik dan Bahan Bakar Rumah Tangga",
-
         var: 2225
-
       },
-
       {
-
         nama: "Perlengkapan, Peralatan dan Pemeliharaan Rutin Rumah Tangga",
-
         var: 2226
-
       },
-
       {
-
         nama: "Kesehatan",
-
         var: 2227
-
-      },
-
-      {
-
+      },      {
         nama: "Informasi, Komunikasi dan Jasa Keuangan",
-
         var: 2228
-
       },
-
       {
-
         nama: "Transportasi",
-
         var: 2229
-
       },
-
       {
-
         nama: "Rekreasi, Olahraga dan Budaya",
-
         var: 2230
-
       },
-
       {
-
         nama: "Pendidikan",
-
         var: 2231
-
       },
-
       {
-
         nama: "Penyediaan Makanan dan Minuman / Restoran",
-
         var: 2232
-
       },
-
       {
-
         nama: "Perawatan Pribadi dan Jasa Lainnya",
-
         var: 2233
-
       }
-
     ];
-
     const fetchKelompok = async (item) => {
-
       let response = null;
-
       for (let i = 0; i < 3; i++) {
-
         try {
-
           console.log(
-
             `REQUEST ${item.nama} | TRY ${i + 1}`
-
           );
-
           response = await axios.get(
-
             `https://webapi.bps.go.id/v1/api/list/model/data/lang/ind/domain/0000/var/${item.var}/th/126/key/${API_KEY}/`,
-
             {
-
               timeout: 4000,
-
               headers: {
-
                 "User-Agent": "Mozilla/5.0"
-
               }
-
             }
-
           );
-
           console.log(
-
             `SUCCESS ${item.nama}`
-
           );
-
           break;
-
         } catch (err) {
-
           console.log(
-
             `FAILED ${item.nama}`,
-
             err.message
-
           );
-
           await new Promise(resolve =>
-
             setTimeout(resolve, 300)
-
           );
-
         }
-
       }
-
       if (!response) {
-
         console.log(
 
           `SKIP ${item.nama}`

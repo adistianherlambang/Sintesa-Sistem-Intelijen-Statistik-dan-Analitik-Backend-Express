a8dee37cc499749130d195c79ff759beed6a1778
Adistian Herlambang <adistian59@gmail.com>
2026-05-21 21:12:27 +0700

    potong test server

--- server.js
+++ server.js
@@ -20,53 +20,6 @@ app.use(express.json());
 
 app.use("/api", api);
 
-const url =
-  "https://webapi.bps.go.id/v1/api/list/model/data/lang/ind/domain/0000/var/2245/th/126/key/6140cf4d3d3cc537fe36176ad6ad09d2/";
-
-app.post("/test", async (req, res) => {
-  try {
-    const response = await fetch(url, {
-      httpsAgent: new https.Agent({ keepAlive: true }),
-      headers: {
-        "User-Agent": "...",
-        Accept: "application/json, text/plain, */*",
-      },
-    });
-
-    if (!response.ok) {
-      const text = await response.text();
-      return res.status(response.status).json({
-        message: `gagal dengan ${response.status}`,
-        body: text,
-      });
-    }
-
-    const text = await response.text();
-
-    if (!text) {
-      return res.status(500).json({
-        message: "empty response",
-      });
-    }
-
-    let data;
-    try {
-      data = JSON.parse(text);
-    } catch (err) {
-      return res.status(500).json({
-        message: "invalid JSON response",
-        raw: text,
-      });
-    }
-
-    return res.json(data);
-  } catch (err) {
-    return res.status(500).json({
-      message: err.message,
-    });
-  }
-});
-
 const port = process.env.PORT;
 app.listen(port, () => {
   console.log("App jalan di ", port);

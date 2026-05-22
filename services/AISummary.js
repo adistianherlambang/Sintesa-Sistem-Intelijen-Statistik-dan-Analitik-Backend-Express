415f81bf28f12070cefe86ee505416b313860ef7
Adistian Herlambang <adistian59@gmail.com>
2026-05-22 16:59:20 +0700

    llm summary

--- services/AISummary.js
+++ services/AISummary.js
@@ -16,8 +16,32 @@ await mongoose.connect(MONGO_URL);
 
 export const AISum = async () => {
   try {
+    const res = await axios.post("https://generativelanguage.googleapis.com/v1beta/models/gemini-flash-latest:generateContent",
+      {
+        "contents": [
+          {
+            "parts": [
+              {
+                "text": "Explain how AI works in 8000 token in bahasa indonesia, with \\n for break, and each paragraph "
+              }
+            ]
+          }
+        ]
+      },
+      {
+        headers: {
+          "Content-Type": "application/json",
+          "X-goog-api-key": process.env.GEMINI_API_KEY
+        }
+      }
+    )
 
+    console.log(
+  res.data.candidates[0].content.parts[0].text
+)
   } catch(err) {
     console.error(err.message)
   }
 }
+
+AISum()
